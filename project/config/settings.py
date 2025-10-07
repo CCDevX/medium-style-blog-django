8d29ab465fcec44135ccc0a77eab73009bd096a8
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-10-07 17:34:13 +0200

    add whitenoise

--- project/config/settings.py
+++ project/config/settings.py
@@ -146,7 +146,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/media'
 
 
 # Default primary key field type
